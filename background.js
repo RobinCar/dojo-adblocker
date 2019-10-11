5acf9ef853bec52377061dc23dfea77b98961600
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-11 15:53:52 +0200

    7. Fetch hosts list

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
+const hostsListUrl = "https://dbl.oisd.nl/basic/";
+
+fetch(hostsListUrl)
+  .then((response) => response.text())
+  .then((responseText) => {
+    let hostsToBlock = responseText
+      .split("\n")
+      .filter(Boolean)
+      .filter((line) => !line.startsWith("#"));
+    console.log(`${hostsToBlock.length} hosts will be blocked`);
+  });
+
 chrome.webRequest.onBeforeRequest.addListener(
-  requestDetails => {
-    console.log(requestDetails.url)
+  (requestDetails) => {
+    console.log(requestDetails.url);
   },
-  { urls: ["<all_urls>"] },
-)
+  { urls: ["<all_urls>"] }
+);
