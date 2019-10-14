dc447ba19611704f4a4734e94a0a84c9c37681f7
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-14 10:52:55 +0200

    8. Block ads from hosts list

--- background.js
+++ background.js
@@ -3,16 +3,20 @@ const hostsListUrl = "https://dbl.oisd.nl/basic/";
 fetch(hostsListUrl)
   .then((response) => response.text())
   .then((responseText) => {
-    let hostsToBlock = responseText
+    let urlsToBlock = responseText
       .split("\n")
       .filter(Boolean)
-      .filter((line) => !line.startsWith("#"));
-    console.log(`${hostsToBlock.length} hosts will be blocked`);
-  });
+      .filter((line) => !line.startsWith("#"))
+      .map((host) => `*://${host}/*`);
 
-chrome.webRequest.onBeforeRequest.addListener(
-  (requestDetails) => {
-    console.log(requestDetails.url);
-  },
-  { urls: ["<all_urls>"] }
-);
+    console.log(`${urlsToBlock.length} hosts will be blocked`);
+
+    chrome.webRequest.onBeforeRequest.addListener(
+      (requestDetails) => {
+        console.log(requestDetails.url);
+        return { cancel: true };
+      },
+      { urls: urlsToBlock },
+      ["blocking"]
+    );
+  });
