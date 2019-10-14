60ff4ff537394893472a5d06c86b0faf325a82bc
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-10-15 01:09:52 +0200

    9. Show number of blocked requests in extension icon

--- background.js
+++ background.js
@@ -1,22 +1,32 @@
 const hostsListUrl = "https://dbl.oisd.nl/basic/";
 
-fetch(hostsListUrl)
-  .then((response) => response.text())
-  .then((responseText) => {
-    let urlsToBlock = responseText
-      .split("\n")
-      .filter(Boolean)
-      .filter((line) => !line.startsWith("#"))
-      .map((host) => `*://${host}/*`);
+let blockedRequestsCount = 0;
 
-    console.log(`${urlsToBlock.length} hosts will be blocked`);
+const fetchListsAndEnableAdBlocking = async () => {
+  const hostsResponse = await fetch(hostsListUrl);
+  const hostsResponseText = await hostsResponse.text();
+  const urlsToBlock = hostsResponseText
+    .split("\n")
+    .filter(Boolean)
+    .filter((line) => !line.startsWith("#"))
+    .map((host) => `*://${host}/*`);
 
-    chrome.webRequest.onBeforeRequest.addListener(
-      (requestDetails) => {
-        console.log(requestDetails.url);
-        return { cancel: true };
-      },
-      { urls: urlsToBlock },
-      ["blocking"]
-    );
-  });
+  console.log(`${urlsToBlock.length} hosts will be blocked`);
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    (requestDetails) => {
+      console.log(requestDetails.url);
+
+      blockedRequestsCount++;
+      chrome.browserAction.setBadgeText({
+        text: blockedRequestsCount.toString(),
+      });
+
+      return { cancel: true };
+    },
+    { urls: urlsToBlock },
+    ["blocking"]
+  );
+};
+
+fetchListsAndEnableAdBlocking();
